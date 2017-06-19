42efa7afe9d7341de4297d4349aad338979451a3
Takashi Imagire <imagire@gmail.com>
2017-06-19 20:41:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 var number = Math.floor(Math.random()*3);
 
-while(true){
+do{
   
 
   var answer = parseInt(window.prompt('数あてゲーム0~2の数字を入力して下さい！！'));
@@ -11,20 +11,16 @@ while(true){
 
   if(answer === number) {
     message = '正解！';
-      document.getElementById('choice').textContent = message;
-    break;
   }
   else if (answer < number){
     message = '残念。もっと大きいです';
-      document.getElementById('choice').textContent = message;
   }
   else if (number < answer){
     message = '残念。もっと小さいです';
-      document.getElementById('choice').textContent = message;
   }
   else{
     message = '0~2の数字を入力してください';
-    document.getElementById('choice').textContent = message;
   }
+  document.getElementById('choice').textContent = message;
 
-}
+}while(answer !== number);
