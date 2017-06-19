98085f9d081e081b666209079971cce73b8dcf37
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-06-19 17:14:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,3 +3,7 @@ window.alert('こんにちは世界！');
 console.log('こんにちは世界！');
 
 document.getElementById('choice').textContent = new date();
+
+var result = window.confirm('準備OK?');
+
+document.getElementById('choice2').textContent = result;
