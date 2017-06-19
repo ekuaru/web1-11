992026febcb51bbc8b36d6ff9fd41074666d2fd0
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-06-19 17:53:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,24 @@
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt('数あてゲーム0~2の数字を入力して下さい！！'));
 
-var message;
-if(answer === number) {
-  message = '正解！';
-}else if (answer < number){
-  message = '残念。もっと大きいです';
-}else if (number < answer){
-  message = '残念。もっと小さいです';
-}else{
-  message = '0~2の数字を入力してください';
+while(number /= answer){
+  
+
+  var answer = parseInt(window.prompt('数あてゲーム0~2の数字を入力して下さい！！'));
+
+
+
+  var message;
+
+  if(answer === number) {
+    message = '正解！';
+    break;
+  }else if (answer < number){
+    message = '残念。もっと大きいです';
+  }else if (number < answer){
+    message = '残念。もっと小さいです';
+  }else{
+    message = '0~2の数字を入力してください';
+  }
+
+  document.getElementById('choice').textContent = message;
 }
-
-document.getElementById('choice').textContent = message;
